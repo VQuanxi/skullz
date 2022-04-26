825df22eb4e2276f10b2b54b4a3e763de260d0ab
VQuanxi <103129080+VQuanxi@users.noreply.github.com>
2022-04-26 17:31:05 +0200

    Update 1

---
+++ skullz.js
@@ -0,0 +1,15 @@
+const { append } = require("express/lib/response");
+
+function handleSubmit(event) {
+   event.preventDefault();
+
+   const data = new FormData(event.target);
+
+   const value = Object.fromEntries(data.entries());
+
+   console.log({ value });
+
+ }
+
+ const form = document.querySelector('form');
+ form.addEventListener('submit', handleSubmit);
